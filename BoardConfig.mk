ca22d2837fcdb6b7898a4001d4c50e573538c682
Stephen Hyde <steve1030uk@gmail.com>
2016-02-13 10:26:26 +0000

    Initial Push of my Pac-5.1 Device tree for G800F

---
+++ BoardConfig.mk
@@ -0,0 +1,109 @@
+#
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# inherit from common smdk3470
+-include device/samsung/smdk3470-common/BoardConfigCommon.mk
+
+# Enable dex-preoptimization to speed up first boot sequence
+ifeq ($(HOST_OS),linux)
+  ifeq ($(TARGET_BUILD_VARIANT),userdebug)
+   ifeq ($(WITH_DEXPREOPT),)
+    WITH_DEXPREOPT := true
+   endif
+  endif
+endif
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := kminiltexx,kminiltedv,kminilteub,kminilte
+
+# Filesystems
+# Note: the BOARD_...IMAGE_PARTITION_SIZE values define the image sizes, 
+# not the partition sizes. So the image sizes can also be lower than the partition size. 
+
+# Real partition sizes
+#BOARD_BOOTIMAGE_PARTITION_SIZE := 10485760
+#BOARD_CACHEIMAGE_PARTITION_SIZE := 314572800
+#BOARD_RECOVERYIMAGE_PARTITION_SIZE := 12582912
+#BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2411724800
+#BOARD_USERDATAIMAGE_PARTITION_SIZE := 12834570240(-16384)
+
+# Image sizes
+BOARD_BOOTIMAGE_PARTITION_SIZE := 10485760
+BOARD_CACHEIMAGE_PARTITION_SIZE := 314572800
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 12582912
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2147483648
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 12833521664
+
+# Include path
+TARGET_SPECIFIC_HEADER_PATH += device/samsung/kminilte/include
+
+# Kernel
+TARGET_KERNEL_CONFIG := kminilte_00_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/kminilte
+
+# CMHW
+BOARD_HARDWARE_CLASS := device/samsung/kminilte/cmhw
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/kminilte/bluetooth
+
+# NFC
+# Chipset: Samsung s3fwrn5
+# Note: as libnfc-nci only supports pn547 and bcm2079x, select pn547 here but use the stock s3fwrn5 hal
+BOARD_NFC_CHIPSET := pn547
+BOARD_NFC_HAL_SUFFIX := universal3470
+
+# Recovery
+TARGET_RECOVERY_DEVICE_DIRS += device/samsung/kminilte
+TARGET_RECOVERY_FSTAB := device/samsung/kminilte/rootdir/etc/fstab.universal3470
+
+# PowerHAL
+TARGET_POWERHAL_VARIANT := exynos3
+
+# Lights
+TARGET_PROVIDES_LIBLIGHT := true
+
+# SELinux
+BOARD_SEPOLICY_DIRS += \
+   device/samsung/kminilte/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    file_contexts \
+    service_contexts \
+    device.te \
+    domain.te \
+    drmserver.te \
+    file.te \
+    gpsd.te \
+    init.te \
+    mediaserver.te \
+    servicemanager.te \
+    system_app.te \
+    system_server.te \
+    vold.te \
+    wpa.te
+
+# Vendor Init
+TARGET_UNIFIED_DEVICE := true
+TARGET_INIT_VENDOR_LIB := libinit_kminilte
+TARGET_LIBINIT_DEFINES_FILE := device/samsung/gardalte/init/init_kminilte.c
+
+# TWRP
+DEVICE_RESOLUTION := 720x1280
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
+TW_NO_REBOOT_BOOTLOADER := true
+TW_HAS_DOWNLOAD_MODE := true
---
+++ README.md
@@ -0,0 +1,43 @@
+# Device configuration for Samsung Galaxy S5 mini SM-G800F (kminilte)
+
+## Spec Sheet
+
+| Feature                 | Specification                     |
+| :---------------------- | :-------------------------------- |
+| CPU                     | Quad-core 1.4 GHz                 |
+| Chipset                 | Exynos 3 Quad 3470                |
+| GPU                     | Mali-400MP4                       |
+| Memory                  | 1.5GB RAM                         |
+| Shipped Android Version | (4.4.2)                           |
+| Storage                 | 16 GB                             |
+| MicroSD                 | Up to 64GB                        |
+| Battery                 | 2100 mAh                          |
+| Dimensions              | 131.1 x 64.8 x 9.1 mm             |
+| Display                 | 720 x 1280 pixels                 |
+| Camera                  | 8 MP, 3264 × 2448 pixels          |
+| Release Date            | August 2014                       |
+
+
+## Device Picture
+
+![Samsung Galaxy S5 mini](http://images.samsung.com/is/image/samsung/de_SM-G800FZBADBT_000246076_Front_blue?$DT-Gallery$ "Samsung Galaxy S5 mini")
+
+## Copyright
+
+```
+#
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+```
